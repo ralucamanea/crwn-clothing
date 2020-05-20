99592126fcbb6a2d4196d9639e9c98561332ca08
Raluca Manea <raluca96ok@gmail.com>
2020-05-20 23:12:30 +0200

    broken our homepage into smaller components, and style the menu-component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './home-page.component';
-import './home-page.styles.scss'
+import HomePage from './pages/homepage/home-page.component';
 
 function App() {
   return (
