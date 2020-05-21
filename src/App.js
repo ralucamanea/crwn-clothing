15ffff65d89f9714c4ae4de4509f3f3230fece8d
Raluca Manea <raluca96ok@gmail.com>
2020-05-21 22:41:22 +0200

    updating app with a shop page, collection preview component, collection item component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/home-page.component';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 //if you use switch (if matches, renders that one, nothing else), does not use exact and vice versa
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage }></Route>
-        <Route exact path='/shop/hats' component={HatsPage}></Route>
+        <Route path='/shop' component={ShopPage}></Route>
       </Switch>
     </div>
   );
