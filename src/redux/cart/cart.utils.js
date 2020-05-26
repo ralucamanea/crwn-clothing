12930c4a8df999457ac84afa0ad6c2fd40cd9974
Raluca Manea <raluca96ok@gmail.com>
2020-05-26 22:07:54 +0200

    add arrows and functionality for increase/decrease quantity at checkout

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -8,4 +8,17 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     } else {
         return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
     }
-} 
+}
+
+export const removeItemFromCartUtil = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(item => item.id === cartItemToRemove.id);
+    if (existingCartItem.quantity === 1) {
+        //remove it
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    } else {
+        //decrease the quantity
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToRemove.id ? { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItem);
+    }
+}
