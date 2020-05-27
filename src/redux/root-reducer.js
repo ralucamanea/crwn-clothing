14b2e567d5621e25cd1ef656fd864a7338b2a778
Raluca Manea <raluca96ok@gmail.com>
2020-05-27 21:21:37 +0200

    add local storage for the cart

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,21 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import {persistReducer} from 'redux-persist';
 
-export default combineReducers({
+//the localStorage of our window broser. Also can import sessionStorage
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    //the reducers that we want to store
+    whiteList: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
