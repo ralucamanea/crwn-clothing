05960d19b81dfb206c1f0d66ac46a0051d8090c3
Raluca Manea <raluca96ok@gmail.com>
2020-05-30 23:16:51 +0200

    delete babel loader

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -23,5 +23,5 @@ export const selectCollectionsForPreview = createSelector(
 export const selectCollection = collectionUrlParam =>
     createSelector(
         [selectCollections],
-        collections => collections[collectionUrlParam]
+        collections => collections.find(col=> col.id === COL_ID_MAP[collectionUrlParam])
     );
