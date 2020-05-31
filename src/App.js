285caaa58637aacb6fa4f9e4b7f9b168aecdac81
Raluca Manea <raluca96ok@gmail.com>
2020-05-31 21:48:21 +0200

    Revert "add collections to firebase - will be removed"
    This reverts commit 555fa06885115a373bf24b60224e3501fc41e34e.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up-page/sign-in-and-sign-up-page.component';
-import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 //if you use switch (if matches, renders that one, nothing else), does not use exact and vice versa
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { createStructuredSelector } from 'reselect';
 import CheckoutPage from './pages/checkout/checkout.component';
-import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser, collectionsArray } = this.props;
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -34,8 +33,6 @@ class App extends React.Component {
         setCurrentUser(userAuth);
       }
     });
-    console.log(collectionsArray);
-    addCollectionAndDocuments(`collections`, collectionsArray.map(({ title, items }) => ({ title, items })));
   }
 
   componentWillUnmount() {
@@ -53,19 +50,18 @@ class App extends React.Component {
           <Route path='/signin' render={() => this.props.currentUser ? (<Redirect to='/'></Redirect>) : (<SignInAndSignUpPage></SignInAndSignUpPage>)}></Route>
           <Route exact path='/checkout' component={CheckoutPage}></Route>
 
-        </Switch>
+</Switch>
       </div >
     );
   }
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  collectionsArray: selectCollectionsForPreview
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user)),
+  setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
